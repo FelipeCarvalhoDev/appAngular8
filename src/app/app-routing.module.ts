93cf8f68df72bff27000454c3b5608d4d515ff2e
Felipe Carvalho <felipelima.web@gmail.com>
2021-09-17 11:16:24 -0300

    Rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AlbumsComponent } from './components/albums/albums.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { TodosComponent } from './components/todos/todos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'posts', component: PostsComponent},
+  { path: 'albums', component: AlbumsComponent},
+  { path: 'todos', component: TodosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PostsComponent } from './components/posts/posts.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './common/header/header.component';
 import { FooterComponent } from './common/footer/footer.component';
+import { PostsComponent } from './components/posts/posts.component';
 import { AlbumsComponent } from './components/albums/albums.component';
 import { TodosComponent } from './components/todos/todos.component';
+import { AlbumsService, PostsService, TodosService } from './services/api.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostsComponent,
     HeaderComponent,
     FooterComponent,
+    PostsComponent,
     AlbumsComponent,
     TodosComponent,
   ],
@@ -24,7 +25,7 @@ import { TodosComponent } from './components/todos/todos.component';
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [HttpClient],
+  providers: [HttpClient, PostsService, AlbumsService, TodosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
