64ce3d4f8bdd99f3f9a8f0663124278ac4426fe0
Felipe Carvalho <felipelima.web@gmail.com>
2021-09-17 01:11:56 -0300

    Arquitetura

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { AlbumsComponent } from './components/albums/albums.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { TesteComponent } from './components/teste/teste.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    HeaderComponent,
+    FooterComponent,
+    AlbumsComponent,
+    TodosComponent,
+    TesteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
